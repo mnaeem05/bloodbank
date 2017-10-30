45869a88d147212be21b68baa84cc1102d6f3ed7
Naeem <Naeem>
2017-10-30 13:32:27 +0500

    adding images

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import twitter from './twitter.png';
 import github from './github.png';
-
-
-
+import instagram from './instagram.png';
+import facebook from './facebook.png';
+import emailus from './emailus.png';
+import linkedin from './linkedin.png';
+import fiverr from './fiverr.png';
+import callus from './callus.png';
 
 export default  class Contact extends Component {
   render() {
@@ -18,12 +21,17 @@ export default  class Contact extends Component {
         <h6 id="box20">we will get back to you as soon as possible!</h6>
         <a href="https://twitter.com/mnaeem05" target="_blank" ><img id="box21" src={twitter} alt="Twitter" title="Twitter"/></a>
         <a href="https://github.com/mnaeem05" target="_blank" ><img id="box22" src={github} alt="GitHub" title="GitHub"/></a>
-        <a href="https://www.instagram.com/mnaeem05/" target="_blank" ></a>
-        <a href="https://www.facebook.com/mnaeem0334" target="_blank" ></a>
-        <a href="mailto:mnaeem0334@gmail.com" ></a>
-        <a href="https://www.linkedin.com/in/muhammadnaeem1" target="_blank" ></a>
-        <a href="https://www.fiverr.com/mnaeem05" target="_blank" ></a>
+        <a href="https://www.instagram.com/mnaeem05/" target="_blank" ><img id="box32" src={instagram} alt="instagram" title="Instagram"/></a>
+        <a href="https://www.facebook.com/mnaeem0334" target="_blank" ><img id="box33" src={facebook} alt="facebook" title="Facebook"/></a>
+        <a href="mailto:mnaeem0334@gmail.com" ><img id="box34" src={emailus} alt="emailus" title="Emailus"/></a>
+        <a href="https://www.linkedin.com/in/muhammadnaeem1" target="_blank" ><img id="box35" src={linkedin} alt="linkedin" title="Linkedin"/></a>
+        <a href="https://www.fiverr.com/mnaeem05" target="_blank" ><img id="box36" src={fiverr} alt="fiverr" title="Fiver"/></a>
+        <img id="box38" src={callus} alt="callus" title="Callus"/>
         </div>
+        <div>
+        <p id="box37">+92 334 3604301</p>
+    </div>
+
         </div>  
     )
   }
