c6cd0606c5f0918ef0aaa8ca67fc28c15f3fc883
M.Naeem <mnaeem0334@gmail.com>
2017-11-07 13:25:46 +0500

    Update Home.js
    remove dashboard

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import signup from './signup.JPG';
 import signin from './signin.JPG';
-import dashboard from './dashboard.jpg';
+// import dashboard from './dashboard.jpg';
 import contact from './contact.png';
-
-const styles = {
-}
 
 export default class Home extends Component {
   render() {
@@ -15,7 +12,6 @@ export default class Home extends Component {
               <ul>
         <li ><Link to="/signup"><img id="box28" src={signup} alt="signup" title="Sign Up"/></Link></li>
         <li><Link  to="/"><img id="box29" src={signin} alt="signin" title="Sign In"/></Link></li>
-        <li><Link  to="/dashboard"><img id="box30" src={dashboard} alt="dashboard" title="Dashboard"/></Link></li>
         <li><Link  to="/contact"><img id="box31" src={contact} alt="contact" title="Contact Us"/></Link></li>
         </ul>
         </div>
